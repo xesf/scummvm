023c30f9824d5a5beb3ed334f28af6d631c10fac
Alex Bevilacqua <alexbevi@gmail.com>
2011-10-24 05:47:06 -0400

    ASYLUM: patch ScummVM to add Asylum engine
    Conflicts:
    
    	base/plugins.cpp
    	configure
    	engines/engines.mk

--- engines/engines.mk
+++ engines/engines.mk
@@ -26,6 +26,11 @@ DEFINES += -DENABLE_AGOS2
 endif
 endif
 
+ifdef ENABLE_ASYLUM
+DEFINES += -DENABLE_ASYLUM=$(ENABLE_ASYLUM)
+MODULES += engines/asylum
+endif
+
 ifdef ENABLE_CGE
 DEFINES += -DENABLE_CGE=$(ENABLE_CGE)
 MODULES += engines/cge
